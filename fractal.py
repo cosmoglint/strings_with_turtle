7fba80733dfcf0a45c3f1b8f3767fc3738142e6e
thedoodler <ramprakashsmail@gmail.com>
2020-10-17 11:19:19 +0530

    added fractal stack

--- fractal.py
+++ fractal.py
@@ -4,7 +4,12 @@
 
 import turtle
 
+ts = turtle.getscreen()
+ts.colormode(255)
 
+t = turtle.Turtle()
+t.speed(5)
+t.left(90)
 
 #some variables
 length = 80
@@ -14,4 +19,18 @@ stack = []
 class branch:
     def __init__(self,length):
         self.length = length
-    def show():
+    def show(self):
+        print(self.length)
+        t.forward(self.length)
+    def revert(self):
+        t.up()
+        t.backward(self.length)
+        t.down()
+    def brancher():
+        rb = branch(self.length/6)
+        lb = branch(self.length/6)
+
+
+root = branch(length)
+root.show()
+turtle.exitonclick()
