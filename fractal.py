7cbdd8f899173049da0e8858efdb6f39c9afa5ca
thedoodler <ramprakashsmail@gmail.com>
2020-10-17 10:50:41 +0530

    added turtle with fractal

---
+++ fractal.py
@@ -0,0 +1,17 @@
+# fractal with python turtle .
+#stack every layer then fill them up
+
+
+import turtle
+
+
+
+#some variables
+length = 80
+stack = []
+
+
+class branch:
+    def __init__(self,length):
+        self.length = length
+    def show():
