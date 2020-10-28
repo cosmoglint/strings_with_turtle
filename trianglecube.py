8e3883f79e06caca575d88ea7c99dfd25c840b2e
thedoodler <ramprakashsmail@gmail.com>
2020-10-28 18:06:43 +0530

    illusion finished

--- trianglecube.py
+++ trianglecube.py
@@ -6,9 +6,16 @@ import math
 t = turtle.Turtle()
 t.speed(0)
 
+change = 10
+iterations = 10
+
+maxsize = 6
+minsize = 1.5
+diffsize = (maxsize-minsize)/iterations
 
 def up_triangle(counts,turt):
-    change = 5
+    global change
+    turt.pensize(minsize)
     side = 0
     print(math.tan(math.radians(30)))
     hyp = change/math.sin(math.radians(30))
@@ -21,16 +28,19 @@ def up_triangle(counts,turt):
         for i in range(1,4):
             turt.forward(side)
             turt.left(120)
-    return (hyp*(counts+1),(10 * (counts+1)), side)
+    return (hyp*(counts),(change * (counts)), side)
 
 def down_triangle(counts,turt):
-    change = 5
+    global change,diffsize
     side = 0
+    curx,cury = turt.pos()
     hyp = change/math.sin(math.radians(30))
     diff = change/math.tan(math.radians(30))
     for j in range(1,counts+1):
+        # t.pensize(1 + ((counts+2 - j)*0.1))
+        t.pensize(diffsize * (counts + 2 - j) * minsize)
         turt.up()
-        turt.goto(0,-hyp * j)
+        turt.goto(curx,cury + hyp * j)
         side += diff*2
         turt.down()
         for i in range(1,4):
@@ -39,6 +49,26 @@ def down_triangle(counts,turt):
 
 
 t.left(60)
-sharp_height,blunt_height,side = up_triangle(10,t)
+for i in range(1,4):
+    t.forward(5)
+    t.left(120)
+
+sharp_height,blunt_height,side = up_triangle(iterations,t)
+
+t.up()
+t.goto(t.pos()[0] + side/2, t.pos()[1] + blunt_height)
+t.down()
+t.left(180)
+down_triangle(iterations,t)
+
+t.up()
+t.goto(t.pos()[0] - side/2, t.pos()[1] + blunt_height)
+t.down()
+down_triangle(iterations,t)
+
+t.up()
+t.goto(t.pos()[0] - side/2, t.pos()[1] - blunt_height - (sharp_height*2))
+t.down()
+down_triangle(iterations,t)
 
 turtle.exitonclick()
