3fab10d0f92175ca04e57ccbba42043784a7cc30
thedoodler <ramprakashsmail@gmail.com>
2020-10-28 17:31:56 +0530

    working triangle logic

--- trianglecube.py
+++ trianglecube.py
@@ -7,41 +7,38 @@ t = turtle.Turtle()
 t.speed(0)
 
 
-def draw_triangle(counts,dir,turt):
-
-    # first height
-    fh = 10;
-    mv = 10/ math.sin(30)
-    change = mv + 10
-    side = fh / math.sin(60)
-
-
-    for i in range(1,counts+1):
-        for j in range(1,4):
+def up_triangle(counts,turt):
+    change = 5
+    side = 0
+    print(math.tan(math.radians(30)))
+    hyp = change/math.sin(math.radians(30))
+    diff = change/math.tan(math.radians(30))
+    for j in range(1,counts+1):
+        turt.up()
+        turt.goto(0,-hyp * j)
+        side += diff*2
+        turt.down()
+        for i in range(1,4):
             turt.forward(side)
             turt.left(120)
-        turt.sety(i*mv)
-        side = (fh + (change*i))/math.sin(60)
+    return (hyp*(counts+1),(10 * (counts+1)), side)
 
-
-# def draw_triangle(counts,dir,turt):
-#
-#     change = 20
-#     turt.sety(change/2)
-#     height = 0
-#     side = height/ math.sin((60))
-#     print(side)
-#
-#     for i in range(counts):
-#         turt.sety(-height/2)
-#         height += change
-#         side = (height) / math.sin(math.degrees(60))
-#         for j in range(1,4):
-#             turt.forward(side)
-#             turt.left(120)
+def down_triangle(counts,turt):
+    change = 5
+    side = 0
+    hyp = change/math.sin(math.radians(30))
+    diff = change/math.tan(math.radians(30))
+    for j in range(1,counts+1):
+        turt.up()
+        turt.goto(0,-hyp * j)
+        side += diff*2
+        turt.down()
+        for i in range(1,4):
+            turt.forward(side)
+            turt.left(120)
 
 
 t.left(60)
-draw_triangle(5,"down",t)
+sharp_height,blunt_height,side = up_triangle(10,t)
 
 turtle.exitonclick()
